dc87c2aa8b55a26de11d9415303efcc78bf785aa
ArthurTemporim <arthurrtl@gmail.com>
2019-02-15 23:18:53 -0200

    Add customer endpoint
    Signed-off-by: ArthurTemporim <arthurrtl@gmail.com>

--- customer/views.py
+++ customer/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Customer
+from .serializers import CustomerSerializer
+
+
+class CustomerList(generics.ListAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+
+class CustomerDetail(generics.RetrieveAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
