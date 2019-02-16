dd49fed18d6c2c89e4339d2d9b675e32f0c9ade2
ArthurTemporim <arthurrtl@gmail.com>
2019-02-16 10:16:15 -0200

    Add cutomer attributes
    Signed-off-by: ArthurTemporim <arthurrtl@gmail.com>

--- customer/models.py
+++ customer/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
 class Customer(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(default='name', max_length=100)
+    cellphone = models.CharField(default='(00) 99999-9999', max_length=100)
+    email = models.CharField(default='name@email.com', max_length=100)
+    cpf = models.CharField(default='000.000.000-00', max_length=100)
+    telegram = models.CharField(default='@name', max_length=100)
+    facebook = models.CharField(default='name', max_length=100)
+    income= models.FloatField(default=0.0)
 
     def __str__(self):
         return self.name
