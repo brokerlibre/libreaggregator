329fac5935fc1bc04bd15252574e5155184d4865
ArthurTemporim <arthurrtl@gmail.com>
2019-02-16 11:07:55 -0200

    Add sale and customer date attribute
    Signed-off-by: ArthurTemporim <arthurrtl@gmail.com>

--- customer/models.py
+++ customer/models.py
@@ -1,13 +1,17 @@
 from django.db import models
 
+import datetime
+
 class Customer(models.Model):
-    name = models.CharField(default='name', max_length=100)
+    name = models.CharField(default='customer', max_length=100)
     cellphone = models.CharField(default='(00) 99999-9999', max_length=100)
     email = models.CharField(default='name@email.com', max_length=100)
     cpf = models.CharField(default='000.000.000-00', max_length=100)
     telegram = models.CharField(default='@name', max_length=100)
     facebook = models.CharField(default='name', max_length=100)
     income= models.FloatField(default=0.0)
+    signupdate= models.DateField(default=datetime.date.today)
+    birthdate= models.DateField(default=datetime.date.today)
 
     def __str__(self):
         return self.name
--- sale/models.py
+++ sale/models.py
@@ -2,12 +2,17 @@ from django.db import models
 from broker.models import Broker
 from customer.models import Customer
 
+import datetime
+
 class Sale(models.Model):
     name = models.CharField(default='sale', max_length=100)
     totalvalue = models.FloatField(default=0.0)
     share = models.FloatField(default=0.0)
     broker = models.ForeignKey(Broker, on_delete=models.CASCADE, default=None)
     Customer = models.ForeignKey(Customer, on_delete=models.CASCADE, default=None)
+    paymentdate = models.DateField(default=datetime.date.today)
+    startdate = models.DateField(default=datetime.date.today)
+    enddate= models.DateField(default=datetime.date.today)
         
 
     def __str__(self):
