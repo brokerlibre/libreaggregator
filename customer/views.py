eeb87c7032d5635f581f1a1f2bcef3c41a690971
ArthurTemporim <arthurrtl@gmail.com>
2019-02-16 12:35:35 -0200

    start post configuration
    Signed-off-by: ArthurTemporim <arthurrtl@gmail.com>

--- customer/views.py
+++ customer/views.py
@@ -1,8 +1,9 @@
 from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from .models import Customer
 from .serializers import CustomerSerializer
-
 
 class CustomerList(generics.ListAPIView):
     queryset = Customer.objects.all()
@@ -12,3 +13,17 @@ class CustomerList(generics.ListAPIView):
 class CustomerDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+
+@api_view(['GET', 'POST'])
+def customer_list(request):
+    if request.method == 'GET':
+        customer = Customer.objects.all()
+        customer = CustomerSerializer(customer, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = CustomerSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
